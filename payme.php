1a68174b9e719b20c8797f76c4aee69f63e9ec7a
CybernatiC <i@fb.uz>
2018-01-14 17:03:41 +0500

    Support php-fpm fix

--- payme.php
+++ payme.php
@@ -167,6 +167,27 @@ FORM;
                 $this->respond($this->error_invalid_json());
             }
 
+			/** 
+				Fix support php-fpm
+			**/
+			
+			
+			if (!function_exists('getallheaders'))
+			{
+					function getallheaders()
+					{
+						$headers = '';
+						 foreach ($_SERVER as $name => $value)
+						 {
+								 if (substr($name, 0, 5) == 'HTTP_')
+								 {
+										 $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+								 }
+						 }
+						 return $headers;
+					}
+			}
+			
             // Authorize client
             $headers = getallheaders();
             $encoded_credentials = base64_encode("Paycom:{$this->merchant_key}");

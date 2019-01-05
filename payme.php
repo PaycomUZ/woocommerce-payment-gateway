dec0f4cef49da1363b2f9cbcc8231bba01132bee
cisspuser <cisspuser@mail.ru>
2019-01-05 18:06:22 +0500

    auto commit

--- payme.php
+++ payme.php
@@ -13,6 +13,22 @@ if (!defined('ABSPATH')) exit;
 
 add_action('plugins_loaded', 'woocommerce_payme', 0);
 
+if (!function_exists('getallheaders')) 
+{ 
+    function getallheaders() 
+    { 
+           $headers = []; 
+       foreach ($_SERVER as $name => $value) 
+       { 
+           if (substr($name, 0, 5) == 'HTTP_') 
+           { 
+               $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value; 
+           } 
+       } 
+       return $headers; 
+    } 
+} 
+
 function woocommerce_payme()
 {
     load_plugin_textdomain('payme', false, dirname(plugin_basename(__FILE__)) . '/lang/');
